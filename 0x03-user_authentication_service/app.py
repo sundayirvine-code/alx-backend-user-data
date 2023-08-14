bd62fafaa18e63fb302ff55ec3edd3b61a3b1044
Irvine Sunday <amugumbiirvine@gmail.com>
2023-08-14 06:46:41 +0000

    created users function in app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,10 +1,25 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+
+AUTH = Auth()
 
 @app.route("/", methods=["GET"])
 def welcome():
     return jsonify({"message": "Bienvenue"})
+
+@app.route("/users", methods=["POST"])
+def users():
+    try:
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"})
+
+    except ValueError as err:
+        return jsonify({"message": "email already registered"}), 400
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
