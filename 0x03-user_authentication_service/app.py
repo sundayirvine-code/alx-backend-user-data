b43fa713d9081f440dba0f53e3ba31b5f8576fc2
Irvine Sunday <amugumbiirvine@gmail.com>
2023-08-15 13:39:28 +0000

    made all files executable

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response, abort
 from auth import Auth
 
 app = Flask(__name__)
@@ -20,6 +20,23 @@ def users():
 
     except ValueError as err:
         return jsonify({"message": "email already registered"}), 400
+
+@app.route("/sessions", methods=["POST"])
+def login():
+    try:
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        if AUTH.valid_login(email, password):
+            session_id = AUTH.create_session(email)
+            response = jsonify({"email": email, "message": "logged in"})
+            response.set_cookie("session_id", session_id)
+            return response
+        else:
+            abort(401)
+
+    except:
+        abort(401)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
