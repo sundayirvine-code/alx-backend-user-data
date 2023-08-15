312123e6aa3ed01350e5b32b05556f06dd4dcd84
Irvine Sunday <amugumbiirvine@gmail.com>
2023-08-15 17:19:02 +0300

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -103,8 +103,11 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
-            reset_token = str(uuid.uuid4())
-            self._db.update_user(user.id, reset_token=reset_token)
-            return reset_token
         except NoResultFound:
-            raise ValueError("User does not exist.")
+            raise ValueError
+
+        reset_token = _generate_uuid()
+
+        self._db.update_user(user.id, reset_token=reset_token)
+
+        return reset_token
